b2aba4db6b6acdaff68f365166b1f7536e249410
Muqeet <muqeetmubeen6@gmail.com>
2026-09-23 14:12:18 -0700

    fix prisma client

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
